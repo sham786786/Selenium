669f55d71bd10bcc86b14b8459a540665efce047
basha440226 <shameer.mulla1980@gmail.com>
2020-05-23 14:27:51 +0100

    master committed

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,8 +4,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("conflict code added 5");
 		System.out.println("conflict code added 6");
-		System.out.println("conflict code added 7");
-		System.out.println("conflict code added 8");
+
 	}
 
 }
